96426e558fcf1390491fc681f83e5c683b432332
joeylam <joey.cm.lam@gmail.com>
2021-10-17 00:36:09 +0800

    working on nested json message wither other lib

--- src/writter.js
+++ src/writter.js
@@ -1,21 +1,43 @@
+import generateExcel from 'json5-to-table'
+
+const moment = require('moment')
+var today = moment()
 
 const fs = require('fs');
 const app = require('express')();
 
+const oputputSchema =[
+    { title: "id", path: "id"},
+    { title: "fund", path: "fund", props: [
+            { title: "fund id", path: "fund_id"},
+            { title: "transaction type", path: "txn_type"},
+            { title: "num. of unit", path: "units"}
+        ]}
+
+]
+
 var json2xlsx = require('json2xlsx');
-const filename = '../output/sample.xlsx';
+const filename = '../output/sample' + today.format('.YYYYMMDD') + '.xlsx';
 
 
 
-app.listen(5050, () => {
-    console.log("app is running on port 5050");
-    writter();
-})
-
+const allUsers = require('../data/sample-nest.json');
 
 var writter = function () {
-    const allUsers = require('../data/sample.json');
-    var xls = json2xlsx.write(filename, 'user', allUsers)
+    var xls = json2xlsx.write(filename, 'user', allUsers, ['id', 'fund'])
 
     console.log('completed!')
 }
+
+var convert2excel = function () {
+    var json2excel = require('json5-to-table')
+
+    generateExcel(allUsers, oputputSchema, {writeTo: filename})
+    console.log('completed!')
+}
+/*
+writter()
+*/
+
+convert2excel()
+
