ce156b6b1685345e812312b35f1c1115d1cfc1e1
joeylam <joey.cm.lam@gmail.com>
2021-10-17 17:42:25 +0800

    try to json2csv lib

--- src/writter.js
+++ src/writter.js
@@ -1,7 +1,7 @@
-import generateExcel from 'json5-to-table'
+const { Parser } = require('json2csv');
 
-const moment = require('moment')
-var today = moment()
+const moment = require('moment');
+var today = moment();
 
 const fs = require('fs');
 const app = require('express')();
@@ -11,33 +11,47 @@ const oputputSchema =[
     { title: "fund", path: "fund", props: [
             { title: "fund id", path: "fund_id"},
             { title: "transaction type", path: "txn_type"},
-            { title: "num. of unit", path: "units"}
-        ]}
+            { title: "num. of unit", path: "unit"}
+        ]},
+    {title: "First name", path: "first Name"},
+    {title: "Last name", path: "lastName"}
 
 ]
 
-var json2xlsx = require('json2xlsx');
-const filename = '../output/sample' + today.format('.YYYYMMDD') + '.xlsx';
+
+const filename = '../output/sample' + today.format('.YYYYMMDD') + '.csv';
 
 
 
-const allUsers = require('../data/sample-nest.json');
+const dataJson = require('../data/sample-nest.json');
 
 var writter = function () {
-    var xls = json2xlsx.write(filename, 'user', allUsers, ['id', 'fund'])
+    var json2xlsx = require('json2xlsx');
+    var xls = json2xlsx.write(filename, 'user', dataJson, ['id', 'fund'])
+    // var write(filename, 'user', dataJson, ['id', 'fund'])
 
     console.log('completed!')
 }
 
 var convert2excel = function () {
     var json2excel = require('json5-to-table')
-
-    generateExcel(allUsers, oputputSchema, {writeTo: filename})
+    generateExcel(dataJson, oputputSchema, {writeTo: filename})
     console.log('completed!')
 }
-/*
-writter()
-*/
 
-convert2excel()
+
+var convert2csv = function () {
+    const json2csvPraser = new Parser();
+    const dataOoutput = json2csvPraser.parse(dataJson);
+
+
+
+    console.log(dataOoutput);
+}
+
+convert2csv()
+
+// writter()
+
+// convert2excel()
 
